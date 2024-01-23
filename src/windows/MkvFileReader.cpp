41ff4f6066a1f958e02c4cfaaf51a170e61dcdf9
Go Watanabe <1570475+wtnbgo@users.noreply.github.com>
2024-01-23 15:46:32 +0900

    windows パス文字コード問題対応

--- src/windows/MkvFileReader.cpp
+++ src/windows/MkvFileReader.cpp
@@ -35,6 +35,18 @@ MkvFileReader::~MkvFileReader()
   Close();
 }
 
+#ifdef _MSC_VER
+#include <windows.h>
+std::wstring utf8_decode(const std::string &str)
+{
+    if( str.empty() ) return std::wstring();
+    int size_needed = MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), NULL, 0);
+    std::wstring wstrTo( size_needed, 0 );
+    MultiByteToWideChar                  (CP_UTF8, 0, &str[0], (int)str.size(), &wstrTo[0], size_needed);
+    return wstrTo;
+}
+#endif
+
 bool
 MkvFileReader::Open(const char *filePath)
 {
@@ -43,7 +55,8 @@ MkvFileReader::Open(const char *filePath)
   }
 
 #ifdef _MSC_VER
-  const errno_t e = fopen_s(&mFile, filePath, "rb");
+  std::wstring wpath = utf8_decode(std::string(filePath));
+  const errno_t e = _wfopen_s(&mFile, wpath.c_str(), L"rb");
   if (e) {
     return false;
   }
